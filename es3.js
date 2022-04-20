55873ab397029bfffc4b23f702f880f734ed408b
dre2599 <casconegames@gmail.com>
2022-04-20 22:16:00 +0200

    fix es3

--- es3.js
+++ es3.js
@@ -1,27 +1,29 @@
-function trova_modello(){
-  
-  var garage = [
-    {marca: 'fiat', modello : '500'},
-    {marca: 'renault', modello : 'clio'},
-    {marca: 'fiat', modello : 'multipla'},
-    {marca: 'renault', modello : 'modus'},
-    {marca: 'citroen', modello : 'c3'},
-    {marca: 'citroen', modello : 'c4'},
-    {marca: 'citroen', modello : 'c5 cross'},
-  ]
+var garage = [
+  {marca: 'fiat', modello : '500'},
+  {marca: 'renault', modello : 'clio'},
+  {marca: 'fiat', modello : 'multipla'},
+  {marca: 'renault', modello : 'modus'},
+  {marca: 'citroen', modello : 'c3'},
+  {marca: 'citroen', modello : 'c4'},
+  {marca: 'citroen', modello : 'c5 cross'},
+]
 
+function trova_modello(){
   var marca = document.getElementById('marca').value.toLowerCase()
-  const risultato = garage.filter(obj => {
-      return obj.marca === marca;
+  const search = garage.filter(obj => {
+    return obj.marca === marca;
   });
-  if (risultato.length == 0){
+
+  if (search.length == 0){
     console.log('inserisci una marca valida')
-  }
-    
-  for (let i = 0; i < risultato.length; i++){
+  } else {
+    document.getElementById('wrap-modello').innerHTML = ''
+    for (let i = 0; i < search.length; i++){
     var element = document.createElement("p");
-    element.innerHTML = risultato[i].modello;
-    document.getElementById('test').append(element);
+    element.setAttribute('id','modello')
+    element.innerHTML = search[i].modello;
+    document.getElementById('wrap-modello').append(element);
+    }
   }
 }
 
